0fef0fdc32c5403ec4c940a7edd22291b2caf1fd
hilalrizqi <63347414+hilalrizqi@users.noreply.github.com>
2020-04-08 19:21:56 +0700

    Update aboutme.php

--- aboutme.php
+++ aboutme.php
@@ -3,4 +3,6 @@
 echo "Nama  : Hilal Rizqi Afrizal;"
 echo "Kelas : XI RPL 2"
 echo "Asal Sekolah : SMK TARUNA BHAKTI DEPOK"
-echo "Alamat : Kp. Tipar Tengah RT/RW. 04/10"
+
+echo "Nomor HP : 081387033055"
+echo "Email : hilalrizqi7@gmail.com"
